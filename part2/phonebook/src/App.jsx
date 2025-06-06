35804295ecab61182dcfbcfb8b239bc25cdf8f73
Leonidas810 <leonardo20010912@gmail.com>
2025-06-06 11:35:38 -0600

    update: part2/ exercises 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, [])
 
   const [personsFilter, setPersonsFilter] = useState({
     filter: "",
@@ -61,9 +64,9 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter onChange={handleOnChangeFilter} value={personsFilter.filter} />
       <h2>Add a new</h2>
-      <PersonForm handleSubmit={handleSubmit} onChange={handleOnChangeInput} valueName={newName.name} valueNumber={newName.number}/>
+      <PersonForm handleSubmit={handleSubmit} onChange={handleOnChangeInput} valueName={newName.name} valueNumber={newName.number} />
       <h2>Numbers</h2>
-      <Persons personsFilter={personsFilter} persons={persons}/>
+      <Persons personsFilter={personsFilter} persons={persons} />
 
     </div>
   )
